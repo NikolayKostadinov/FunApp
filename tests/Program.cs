6ccc638e78132e4d2c5abd86c6375d8de7e30439
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2019-06-04 16:17:39 +0300

    Created SandBox

---
+++ tests/Program.cs
@@ -0,0 +1,66 @@
+﻿//  ------------------------------------------------------------------------------------------------
+//   <copyright file="Program.cs" company="Business Management System Ltd.">
+//       Copyright "2019" (c), Business Management System Ltd. 
+//       All rights reserved.
+//   </copyright>
+//   <author>Nikolay.Kostadinov</author>
+//  ------------------------------------------------------------------------------------------------
+
+namespace SаndBox
+{
+    #region Using
+
+    using System;
+    using System.IO;
+    using System.Linq;
+    using FunApp.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    #endregion
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            // Seed data on application startup
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<FunAppContext>();
+                dbContext.Database.Migrate();
+            }
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            // TODO: Code here
+            var db = serviceProvider.GetService<FunAppContext>();
+            Console.WriteLine(db.Users.Count());
+        }
+
+        private static void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceCollection.AddDbContext<FunAppContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
